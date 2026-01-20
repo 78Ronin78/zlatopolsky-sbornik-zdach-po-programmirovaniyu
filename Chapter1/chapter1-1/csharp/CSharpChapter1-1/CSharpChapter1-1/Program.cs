55e92f70229b18a6dbcb1874c7b64b83af677165
Dmitriy Ryzhov <superdiman_1994@mail.ru>
2026-01-20 13:12:44 +0800

    build: создать проект c#

---
+++ Chapter1/chapter1-1/csharp/CSharpChapter1-1/CSharpChapter1-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CSharpChapter1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            WriteLine("Число Пи с точностью до сотых: {0}", pi);
+        }
+    }
+}
